e2cc1f89a1950c79a56eace17a1221cf79125717
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-06 13:20:56 +0100

    caching

--- utils/cacheMiddleware.ts
+++ utils/cacheMiddleware.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import redis from 'redis';
 
 const client = redis.createClient();
 
 // Cache middleware
-export const cacheMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const cacheMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const key = req.originalUrl; // Use the request URL as the cache key
 
-  client.get(key, (err, data) => {
+  client.get(key, (err: Error | null, data: string | null) => {
     if (err) {
       console.error('Error retrieving data from cache:', err);
       next();
@@ -18,10 +18,10 @@ export const cacheMiddleware = (req: express.Request, res: express.Response, nex
       res.status(200).json(JSON.parse(data)); // Serve data from cache
     } else {
       console.log('Data not found in cache');
-      res.sendResponse = res.send;
+      const sendResponse = res.send.bind(res); // Bind res.send to the response object
       res.send = (body) => {
-        client.setex(key, 3600, JSON.stringify(body)); // Cache data for 1 hour
-        res.sendResponse(body);
+        client.setEx(key, 3600, JSON.stringify(body)); // Cache data for 1 hour using setEx
+        sendResponse(body); // Send the response
       };
       next();
     }
