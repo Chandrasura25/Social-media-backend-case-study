63007dffdf8187cf72d5dd85f83a0e6369e0738c
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-06 20:38:41 +0100

    likes and comments

---
+++ src/utils/multerConfig.ts
@@ -0,0 +1,30 @@
+import multer from 'multer';
+
+// Configure multer for file upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix); // File naming convention
+  }
+});
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Only JPEG and PNG images are allowed'));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // Limit file size to 5MB
+  },
+  fileFilter: fileFilter
+});
+
+export default upload;
