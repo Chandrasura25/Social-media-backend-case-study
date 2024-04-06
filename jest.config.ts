c5d8a518c83cf0004e6b6b85f8f5ba7c1d57af07
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-06 20:37:54 +0100

    Testing

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', 'src'],
+};
+
+export default config;
